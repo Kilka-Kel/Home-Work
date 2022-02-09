bc151a40232e8f0b4b56d75112f92ab74edf9d9d
Kilka <Keilija@mail.ru>
2022-02-09 23:56:39 +0400

    001 вывести квадрат числа

---
+++ Path1/01/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+double namderA = 1;
+string s;
+Console.Write("Введите число А (можно дробное через ,) - ");
+s = Console.ReadLine();
+namderA = Convert.ToDouble(s);
+Console.Write("A*A=");
+Console.WriteLine(namderA * namderA);
