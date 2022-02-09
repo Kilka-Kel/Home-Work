d5ff0730c2eee98e053c3a78ee14c3c01b8b9c8d
Kilka <Keilija@mail.ru>
2022-02-10 00:40:28 +0400

    002 проверка квадратности B

--- Path1/02/Program.cs
+++ Path1/02/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+Console.Clear();
+Console.WriteLine("По двум заданным числам проверим, является ли первое квадратом второго");
+
+double namderA = 1;
+double namderB = 1;
+string s;
+Console.Write("Введите число А (можно дробное через ,) - ");
+s = Console.ReadLine();
+namderA = Convert.ToDouble(s);
+namderA=namderA * namderA;
+
+Console.Write("Введите число B (можно дробное через ,) - ");
+s = Console.ReadLine();
+namderB = Convert.ToDouble(s);
+
+if (namderA == namderB)
+{
+   Console.ForegroundColor=ConsoleColor.DarkGreen;
+   Console.Write("Вы ввели число B являющееся квадратом А ");
+}
+else
+{
+    Console.ForegroundColor=ConsoleColor.DarkRed;
+    Console.Write("НЕТ! Число B не являеется квадратом А");
+
+}    
+// Console.Write("A*A=");
+// Console.WriteLine(namderA);
+
