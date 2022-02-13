ca1ff7f64b2b3ae131382478ceaab783e640e6ed
Kilka <Keilija@mail.ru>
2022-02-13 19:27:46 +0400

    Добавила новое решение для 031 и 032

---
+++ Part4/31/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("Задать массив из 8 элементов и вывести их на экран");
+int [] Array = {1,2,3,4,5,6,7,8};
+// int step = 0;
+// int size = Array.Length;
+//Вывод через for
+for (int step = 0;step<Array.Length; step++)
+    System.Console.Write($"{Array[step]} ");
+
+//Вывод через while
+// while (step!=size)
+// {
+//     System.Console.WriteLine(Array[step]);
+//     step++;
+// }
---
+++ Part4/32/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран");
+
+int[] Array = new int[8]; //new Random().Next(0,2)
+
+void FillArray(int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(0, 2);
+        index++;
+    }
+}
+
+void PrintArray(int[] coll)
+{
+    int lenghtPrint = coll.Length;
+    int indexPrint = 0;
+    while (indexPrint < lenghtPrint)
+    {
+        System.Console.WriteLine(coll[indexPrint]);
+        indexPrint++;
+    }
+}
+
+FillArray(Array);
+PrintArray(Array);
+
+//Так же решеине через for
+for (int step = 0; step < Array.Length; step++)
+    Array[step] = new Random().Next(0, 2);
+    
+for (int step = 0; step < Array.Length; step++)
+    System.Console.Write($"{Array[step]} ");
