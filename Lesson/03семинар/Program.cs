7aa290d78c36f1142669f2c0d13dc0b6a2a2ff8e
Kilka <Keilija@mail.ru>
2022-02-18 00:54:24 +0400

    lesson 3 update

---
+++ Lesson/03семинар/Program.cs
@@ -0,0 +1,14 @@
+﻿// Пересобираем строчку в обратном порядке
+Console.WriteLine("Пересобираем строчку в обратном порядке");
+
+
+string s = Console.ReadLine();
+string result = string.Empty;
+
+for (int i = s.Length-1; i >=0; i--)
+{
+    result = result + s[i];
+    // System.Console.Write(s[i]);
+
+}
+System.Console.WriteLine(result);
