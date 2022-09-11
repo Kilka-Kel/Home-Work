cf6e569b36c368735e71e2813dfb17cf9d746243
Kilka <Keilija@mail.ru>
2022-09-11 15:54:32 +0300

    Переделала вывод методом

--- Итоговая проверочная/Program.cs
+++ Итоговая проверочная/Program.cs
@@ -1,26 +1,30 @@
 ﻿Console.Clear();
 Console.ForegroundColor = ConsoleColor.DarkBlue;
 Console.WriteLine("Сначала выводим наш массив");
-// int [] Array = {1,2,3,4,5,6,7,8}; // потом удали всё
-// int step = 0;
-// int size = Array.Length;
-//Вывод через for
-// for (int step = 0;step<Array.Length; step++)
-//     System.Console.Write($"{Array[step]} ");
 
-//Вывод через while
-// while (step!=size)
+
+
+string [] Str= { "Mon", "Tuesd", "Wednesday", "Thirsday", "Friday", "Saturday","Sun" };
+int size = Str.Length;
+// int step2 = 0;
+// while (step2!=size)
 // {
-//     System.Console.WriteLine(Array[step]);
-//     step++;
+
+//     System.Console.Write($"{Str[step2]} ");
+//     step2++;
 // }
-int step2 = 0;
 
-string [] str= { "Mon", "Tuesd", "Wednesday", "Thirsday", "Friday", "Saturday","Sun" };
-int size = str.Length;
-while (step2!=size)
+void PrintArray(string[] coll)//печатаем массив
 {
-
-    System.Console.Write($"{str[step2]} ");
-    step2++;
+    int lenghtPrint = coll.Length;
+    int indexPrint = 0;
+    while (indexPrint < lenghtPrint)
+    {
+        System.Console.Write($"{coll[indexPrint]} ");
+        indexPrint++;
+    }
+        System.Console.WriteLine();
 }
+
+PrintArray(Str);
+
